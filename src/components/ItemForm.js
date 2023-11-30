6c0d5744bbacc6c9f8d83757b6d6a02439b3e1bd
mmwandera <mmwandera74@gmail.com>
2023-11-30 13:08:49 +0300

    completed assignment

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,6 +1,19 @@
 import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
+// There is a new component called ItemForm that will allow us to add new items to our shopping list. When the form is submitted, a new item should be created and added to our list of items.
+// Make all the input fields for this form controlled inputs, so that you can access all the form data via state. When setting the initial state for the <select> tag, use an initial value of "Produce" (since that's the first option in the list).
+
+// Handle the form's submit event, and use the data that you have saved in state to create a new item object with the following properties:
+// const newItem = {
+//   id: uuid(), // the `uuid` library can be used to generate a unique id
+//   name: itemName,
+//   category: itemCategory,
+// };
+
+// Add the new item to the list by updating state. To get the test passing, you'll need to use a prop called onItemFormSubmit as a callback and pass the new item to it.
+
+
 function ItemForm({ onItemFormSubmit }) {
   const [formData, setFormData] = useState({
     name: "",
